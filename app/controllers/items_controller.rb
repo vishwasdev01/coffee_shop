d19e5553cf9210fa0a54df5199b3250c33c8c088
Vishwas N <vishwanah112@gmail.com>
2023-06-01 15:04:17 +0530

    Add logic for tax_rate, discount

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  skip_before_action :verify_authenticity_token
 
 	def index
     items = Item.all
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,40 +1,21 @@
 class OrdersController < ApplicationController
-  skip_before_action :verify_authenticity_token
 
-	def index
-	  orders = Order.all
-	  render json: orders
-	end
+  def index
+    orders = Order.all
+    render json: orders
+  end
 
-	def show
-    order = Order.find_by(id: params[:id])
+  def show
+    order = find_order_by_id
     if order.present?
-	    render json: order.to_json(include: :items)
-	  else
-	  	render json: {message: "Id not found"}, status: :unprocessable_entity
-	  end
-	end
+      render_order_with_items_and_discount(order)
+    else
+      render json: { message: "Order not found" }, status: :unprocessable_entity
+    end
+  end
 
   def create
-    order = Order.new(order_params)
-    item_names = params[:item_names]
-    items = []
-    total = 0.0
-    
-    item_names.each do |name|
-      item = Item.find_by(name: name)
-      if item.nil?
-        render json: { error: "Item '#{name}' not found." }, status: :unprocessable_entity
-        return
-      end
-      items << item
-      total += item.price
-    end
-    
-    order.items << items if items.present?
-    order.total = total
-    order.completed = false
-    
+    order = build_order
     if order.save
       render json: order, status: :created
     else
@@ -42,19 +23,73 @@ class OrdersController < ApplicationController
     end
   end
 
-	def update
-	  order = Order.find(params[:id])
-	  if order.update(completed: true)
-	    render json: order
-	  else
-	    render json: order.errors, status: :unprocessable_entity
-	  end
-	end
+  def update
+    order = find_order_by_id
+    if order.update(completed: true)
+      render json: order
+    else
+      render json: order.errors, status: :unprocessable_entity
+    end
+  end
 
-	private
+  private
 
-	def order_params
-	  params.require(:order).permit(:customer_name, :total, :completed)
-	end
+  def order_params
+    params.require(:order).permit(:customer_name)
+  end
 
+  def item_names
+    params[:item_names]
+  end
+
+  def find_order_by_id
+    Order.find_by(id: params[:id])
+  end
+
+  def render_order_with_items_and_discount(order)
+    items = order.items.map { |item| apply_discount(item) }
+
+    render json: { order: order, items: items }
+  end
+
+  def build_order
+    order = Order.new(order_params)
+    items = []
+    total = 0.0
+
+    item_names.each do |name|
+      item = Item.find_by(name: name)
+      if item.nil?
+        order.errors.add(:base, "Item '#{name}' not found.")
+        return order
+      end
+
+      items << item
+      total += calculate_item_total(item)
+    end
+
+    order.items << items if items.present?
+    order.total = total
+    order.completed = false
+    order
+  end
+
+  def calculate_item_total(item)
+    price_with_discount = item.discount ? item.price * (1 - discount_percentage(item) / 100.0) : item.price
+    price_with_tax = price_with_discount * (1 + item.tax_rate / 100.0)
+    price_with_tax
+  end
+
+  def discount_percentage(item)
+    item.discount ? 20 : 0  # Example: 20% discount if 'discount' is true, 0% discount otherwise
+  end
+
+  def apply_discount(item)
+    if item.discount
+      discounted_price = item.price * (1 - discount_percentage(item) / 100.0)
+      item.as_json.merge(discounted_price: discounted_price, tax_rate: item.tax_rate)
+    else
+      item.as_json.merge(tax_rate: item.tax_rate)
+    end
+  end
 end
